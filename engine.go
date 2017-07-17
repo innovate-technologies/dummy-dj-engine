368758dcddc9375c9b14b4b13c5f12451e6db474
Maartje Eyskens <meyskens@me.com>
2017-07-17 16:03:30 +0200

    initial commit

---
+++ engine.go
@@ -0,0 +1,45 @@
+package dummyengine
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/chuckpreslar/emission"
+	"github.com/innovate-technologies/DJ/data"
+)
+
+// Engine is the dummy engine
+type Engine struct {
+}
+
+// New  returns a new engine
+func New() Engine {
+	return Engine{}
+}
+
+// Start starts the engine
+func (e Engine) Start(events *emission.Emitter) {
+	fmt.Println("Started the engine")
+}
+
+// PutQueue replaced the queue
+func (e Engine) PutQueue(songs []data.Song) {
+	for _, song := range songs {
+		fmt.Println("queueing", song.ID)
+	}
+}
+
+// Skip skips the current next song
+func (e Engine) Skip() {
+	fmt.Println("Skipped song")
+}
+
+// PipeLive piped the reader to the stream
+func (e Engine) PipeLive(r io.Reader) {
+	fmt.Println("Piping a live stream")
+}
+
+// SetMetadata sets the stream metadata
+func (e Engine) SetMetadata(song string) {
+	fmt.Println("Setting metadata", song)
+}
